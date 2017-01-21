6d0136218afc478309f8e1d9cec8157e36522d9b
yura95st <yura95st@gmail.com>
2017-01-21 18:53:26 +0200

    Implement main app logic.

---
+++ app/main.ts
@@ -0,0 +1,16 @@
+import { Config } from "./config";
+import { BoardService } from "./board.service";
+import { BoardStyleService } from "./board-style.service";
+
+let boardService = new BoardService(Config.boardConfigStringRegExp, Config.cardContentXPath, Config.boardIdRegExp);
+let boardStyleService = new BoardStyleService(Config.styleContainerId);
+
+setInterval(() => {
+    if (boardService.isBoardUrl(document.URL)) {
+        let boardConfig = boardService.getBoardConfig();
+        boardStyleService.applyStyle(boardConfig);
+    }
+    else {
+        boardStyleService.removeStyle();
+    }
+}, 1000);
