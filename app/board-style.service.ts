89e1295b7fb92a68a2f9735c18c711fe24a64756
yura95st <yura95st@gmail.com>
2017-01-21 18:52:38 +0200

    Add boardStyle service.

---
+++ app/board-style.service.ts
@@ -0,0 +1,76 @@
+﻿import { BoardConfig } from "./board-config";
+import { BoardBackground } from "./board-background";
+
+export class BoardStyleService {
+
+    constructor(private styleContainerId: string) {
+    }
+
+    applyStyle(boardConfig: BoardConfig) {
+        if (!boardConfig) {
+            this.removeStyle();
+            return;
+        }
+
+        let styleContainer = this.getStyleContainer();
+
+        if (!styleContainer) {
+            styleContainer = this.createStyleContainer();
+        }
+
+        let styleString = this.getStyleString(boardConfig);
+
+        if (styleString != styleContainer.innerText) {
+            styleContainer.innerText = styleString;
+        }
+    }
+
+    removeStyle() {
+        let styleContainer = this.getStyleContainer();
+
+        if (styleContainer) {
+            document.head.removeChild(styleContainer);
+        }
+    }
+
+    private getStyleContainer(): HTMLElement {
+        return document.getElementById(this.styleContainerId);
+    }
+
+
+    private createStyleContainer(): HTMLElement {
+        let styleContainer = document.createElement("style");
+
+        styleContainer.setAttribute("id", this.styleContainerId);
+
+        document.head.appendChild(styleContainer);
+
+        return styleContainer;
+    }
+
+    private getStyleString(boardConfig: BoardConfig): string {
+        let result = this.getBackgroundStyleString(boardConfig.background);
+
+        return result;
+    }
+
+    private getBackgroundStyleString(boardBackground: BoardBackground): string {
+        let result = "";
+
+        if (boardBackground) {
+            result += "body {";
+
+            if (boardBackground.image) {
+                result += `background-image: url(${boardBackground.image}) !important;`;
+            }
+
+            if (boardBackground.color) {
+                result += `background-color: ${boardBackground.color} !important;`;
+            }
+
+            result += "}";
+        }
+
+        return result;
+    }
+}
