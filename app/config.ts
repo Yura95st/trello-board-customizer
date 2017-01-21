bbcef4685c943c4ab6f437bf5cde251861d93940
yura95st <yura95st@gmail.com>
2017-01-21 18:51:26 +0200

    Add config file.

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,9 @@
 export class Config {
+    static readonly cardContentXPath = "//*[@id='board']/div/div/div[2]/div[1]/div[3]/a/text()";
+    
+    static readonly boardConfigStringRegExp = /@@boardConfig=({[\s\S]*})/g;
+
+    static readonly styleContainerId = "trello-board-cutomizer-style";
+
+    static readonly boardIdRegExp = /:\/\/trello.com\/b\/([a-zA-Z0-9]*)\//g;
 }
