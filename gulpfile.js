76e296eeb41d49a8985503391f735920b89b4702
yura95st <yura95st@gmail.com>
2017-01-23 02:47:46 +0200

    Update tasks in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,56 @@
-var browserify = require("browserify");
-var buffer = require("vinyl-buffer");
-var gulp = require("gulp");
-var gutil = require("gulp-util");
-var source = require("vinyl-source-stream");
-var sourcemaps = require("gulp-sourcemaps");
-var tsify = require("tsify");
-var uglify = require("gulp-uglify");
-var watchify = require("watchify");
+var browserify = require("browserify"),
+    buffer = require("vinyl-buffer"),
+    del = require("del"),
+    gulp = require("gulp"),
+    gutil = require("gulp-util"),
+    runSequence = require('run-sequence'),
+    source = require("vinyl-source-stream"),
+    sourcemaps = require("gulp-sourcemaps"),
+    tsify = require("tsify"),
+    uglify = require("gulp-uglify");
 
-var watchedBrowserify = watchify(browserify({
-    basedir: ".",
-    debug: true,
-    entries: ["app/main.ts"],
-    cache: {},
-    packageCache: {}
-}).plugin(tsify));
+var config = {
+    entrypoint: "./app/main.ts",
+    bundleName: "bundle.js",
+    outputDirPath: "./dist",
+    assets: [
+        "./images/**/*.png",
+        "./manifest.json"
+    ]
+};
 
-function bundle() {
-    return watchedBrowserify
+gulp.task("clean", function () {
+    return del.sync(config.outputDirPath);
+});
+
+gulp.task("copy-assets", function () {
+    return gulp.src(config.assets, { "base": "." })
+        .pipe(gulp.dest(config.outputDirPath));
+});
+
+gulp.task("bundle", function () {
+    return browserify({
+        basedir: ".",
+        debug: true,
+        entries: [config.entrypoint],
+        cache: {},
+        packageCache: {}
+    })
+        .plugin(tsify)
         .bundle()
-        .pipe(source("bundle.js"))
+        .pipe(source(config.bundleName))
         .pipe(buffer())
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify())
         .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest("dist"));
-}
+        .pipe(gulp.dest(config.outputDirPath));
+});
 
-gulp.task("default", bundle);
+gulp.task("default", function (callback) {
+    runSequence("clean", ["copy-assets", "bundle"], callback);
+});
 
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", gutil.log);
+gulp.task("watch", function () {
+    gulp.watch(config.assets, ["copy-assets"]);
+    gulp.watch("**/*.ts", ["bundle"]);
+});
